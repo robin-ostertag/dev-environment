fc8ef982dab879ef79f3eec85ff532d3c68c4a8b
Robin Ostertag <robin.ostertag@gmx.de>
2026-03-02 14:55:39 +0100

    sync gitconfig scripts

---
+++ scripts/sync-gitconfig.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PDE_GITCONFIG="$HOME/dev-environment/.gitconfig"
+
+# overide the entries for core and advice
+# Not a real migration. If a previous version of ribyns-PDE_GITCONFIG had values and are not deleted
+# they wont be removed
+# new ones will be added
+# existing ones overidden
+git config --file "$PDE_GITCONFIG" --get-regexp '^(core|advice|pull|push)\.' | while read -r key value; do
+	git config --global "$key" "$value"
+done
+
+# discard all aliases
+git config --global --remove-section alias 2>/dev/null
+
+# re-add them all
+git config --file "$PDE_GITCONFIG" --get-regexp '^alias\.' | while read -r key value; do
+	key="${key#alias.}"
+	git config --global alias."$key" "$value"
+done
+
+echo "successfully synced .gitconfig"
+cat "$PDE_GITCONFIG"
