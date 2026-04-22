5ab79634f2a0070b8e5fc609c0cad539208953a1
Robin Ostertag <robin.ostertag@gmx.de>
2026-04-22 18:16:04 +0200

    prompt if tools should be installed

--- install.sh
+++ install.sh
@@ -4,16 +4,35 @@ source "scripts/utils.sh"
 # source to get access to $os variable
 source "scripts/detect_os.sh"
 
+# Helper function: asks and then executes the command if confirmed
+run_if_confirmed() {
+    local step_name="$1"
+    local cmd="$2"
+
+    # Changed prompt to (Y/n) to indicate 'Y' is the default
+    read -r -p "Install $step_name? (Y/n): " choice
+    
+    case "$choice" in 
+      # Matches empty string (Enter) or any variation of 'y'
+      ""|[yY][eE][sS]|[yY]) 
+        eval "$cmd" 
+        ;;
+      *) 
+        info "Skipped Install: $step_name"
+        ;;
+    esac
+}
+
 # Install dependencies
 
-# Shared scripts
-./scripts/nvm-v0.40.4-install.sh
+# --- Shared installations ---
+run_if_confirmed "nvm" "./scripts/nvm-v0.40.4-install.sh"
 
-# Call OS-specific scripts
+# --- OS-Specific installations ---
 if [ "$os" = "linux" ]; then
-  ./scripts/install-omz.sh
+    run_if_confirmed "Oh My Zsh" "./scripts/install-omz.sh"
 elif [ "$os" = "windows" ]; then
-  ./scripts/sync-bashrc.sh "$os"
+    run_if_confirmed "Bashrc Sync" "./scripts/sync-bashrc.sh \"$os\""
 fi
 
 # sync config files
