8a05c33850efd0fbb69431fd08c8ced75ca7e5e3
Robin Ostertag <robin.ostertag@gmx.de>
2026-04-22 18:06:07 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 source "scripts/utils.sh"
 
+# source to get access to $os variable
+source "scripts/detect_os.sh"
+
+# Install dependencies
+
+# Shared scripts
+./scripts/nvm-v0.40.4-install.sh
+
+# Call OS-specific scripts
+if [ "$os" = "linux" ]; then
+  ./scripts/install-omz.sh
+elif [ "$os" = "windows" ]; then
+  ./scripts/sync-bashrc.sh "$os"
+fi
+
+# sync config files
+"$HOME/dev-environment/sync.sh"
