7c0ef00bbed89a602b131fc9cfb69ac40506d400
Robin Ostertag <robin.ostertag@gmx.de>
2026-04-22 18:06:01 +0200

    use detect_os script

--- sync.sh
+++ sync.sh
@@ -1,49 +1,16 @@
 #!/usr/bin/env bash
 source "scripts/utils.sh"
 
-# Auto-detect OS
-case "$(uname -s)" in
-  Linux*)     detected_os="linux" ;;
-  Darwin*)    detected_os="linux" ;;  # macOS → treat like linux bash-wise
-  CYGWIN*|MINGW*|MSYS*) detected_os="windows" ;;
-  *)          detected_os="unknown" ;;
-esac
-
-info "Detected OS: $detected_os"
-
-# let the user override the detected OS
-read -r -p "Use detected OS? (Y/n): " confirm
-
-if [[ "$confirm" =~ ^[Nn]$ ]]; then
-  echo "Select your operating system:"
-  echo "1) Linux"
-  echo "2) Windows"
-  read -r -p "Enter choice [1-2]: " choice
-
-  case $choice in
-    1) os="linux" ;;
-    2) os="windows" ;;
-    *) error "Invalid choice. Exiting."; exit 1 ;;
-  esac
-else
-  os="$detected_os"
-fi
-
-if [ "$os" = "unknown" ]; then
-  error "Could not detect OS. Please choose manually."
-  exit 1
-fi
-
-info "Using OS: $os"
+# source to get access to $os variable
+source "scripts/detect_os.sh"
 
 # Shared scripts
-./scripts/nvm-v0.40.4-install.sh
 ./scripts/sync-gitconfig.sh
 
 # Call OS-specific scripts
 if [ "$os" = "linux" ]; then
   ./scripts/sync-zshrc.sh
-  ./scripts/install-omz.sh
+  ./scripts/sync-p10k_zsh.sh
 elif [ "$os" = "windows" ]; then
   ./scripts/sync-bashrc.sh "$os"
 fi
