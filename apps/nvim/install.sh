34e6a157518aa6eb0518255ee4417c8862bae25b
Robin Ostertag <robin.ostertag@gmx.de>
2026-08-12 17:31:38 +0200

    install script for nvim

---
+++ apps/nvim/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+"$DEV_ENV/apps/nvim/build-from-source.sh"
--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ run_if_confirmed ".NET" "./scripts/install-dotnet.sh"
 # --- OS-Specific installations ---
 if [ "$os" = "linux" ]; then
 	./scripts/ensure_homebrew_installed.sh
-    run_if_confirmed "Build Neovim (nvim) from source" "./scripts/build-nvim-from-source.sh"
+    run_if_confirmed "Build Neovim (nvim) from source" "$DEV_ENV/apps/nvim/install.sh"
     run_if_confirmed "Apt installs" "./scripts/install-apt.sh"
     run_if_confirmed "Oh My Zsh" "./scripts/install-omz.sh"
 elif [ "$os" = "windows" ]; then
