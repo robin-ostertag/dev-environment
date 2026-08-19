b92b1613b4414d16f7a4300d1020b2386328294c
Robin Ostertag <robin.ostertag@gmx.de>
2026-08-19 11:09:49 +0200

    edit-dotnet-user-secrets

--- scripts/install-apt.sh
+++ scripts/install-apt.sh
@@ -5,10 +5,12 @@
 # shfmt: shell format
 # shell check: shell linting
 # btop: TaskManager
+# rg (ripgrep): search across all/multiple files
 
 sudo apt install -y \
 	fzf \
 	fd-find \
 	shfmt \
 	shellcheck \
-	btop
+	btop \
+	ripgrep
